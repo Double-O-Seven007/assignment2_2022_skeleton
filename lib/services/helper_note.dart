c6d6c4860997dd62bb62fd1401367141da5de765
Double-O-Seven007 <84271473+Double-O-Seven007@users.noreply.github.com>
2022-07-02 17:49:50 +0200

    Please implement 'firstLoad' then push the repo back

--- lib/services/helper_note.dart
+++ lib/services/helper_note.dart
@@ -1,21 +1,38 @@
 import 'package:assignment2_2022/models/note.dart';
+import 'package:assignment2_2022/services/notes_service.dart';
+import 'package:assignment2_2022/services/user_service.dart';
 import 'package:assignment2_2022/widgets/dialogues.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void refreshNotesInUI(BuildContext context) async {}
+void refreshNotesInUI(BuildContext context) async {
+  String result = await context.read<NotesServie>().getNotes(
+      context.read<UserService>().currentUser!.email,
+      firstLoad); //TODO push repo after implementing this.
+}
 
 void saveNotesInUI(BuildContext context) async {}
 
-void createNotesInUI(BuildContext context,
-    {required TextEditingController titleController,
-    required emailController,
-    required messageController}) async {
+void createNotesInUI(
+  BuildContext context, {
+  required TextEditingController titleController,
+  required emailController,
+  required messageController,
+}) async {
   if (titleController.text.isEmpty) {
-    showSnackbar(context, 'Please Enter a note then save!');
+    showSnackbar(context, 'Please Enter a note first then save!');
   } else {
     Note note = Note(
-        title: titleController.text.trim(),
-        email: emailController,
-        message: emailController);
+      title: titleController.text.trim(),
+      email: emailController,
+      message: emailController,
+    );
+    if (context.read<NotesServie>().notes.contains(note)) {
+      showSnackbar(context, 'Duplicate value. Please try again.');
+    } else {
+      titleController.text = '';
+      context.read<NotesServie>().createNote(note);
+      Navigator.pop(context);
+    }
   }
 }
