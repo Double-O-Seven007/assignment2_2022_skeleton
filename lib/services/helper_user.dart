9210699249121267802128243fd86e255cc5ed77
Double-O-Seven007 <84271473+Double-O-Seven007@users.noreply.github.com>
2022-07-03 12:33:42 +0200

    Commit 4, test (at your office)

--- lib/services/helper_user.dart
+++ lib/services/helper_user.dart
@@ -1,12 +1,24 @@
+import 'package:assignment2_2022/routes/route_manager.dart';
+import 'package:assignment2_2022/services/notes_service.dart';
+import 'package:assignment2_2022/widgets/dialogues.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void createNewUserInUI(BuildContext context,
     {required String email,
     required String password,
     required String name}) async {}
-
 void loginUserInUI(BuildContext context,
-    {required String email, required String password}) async {}
+    {required String email, required String password}) async {
+  FocusManager.instance.primaryFocus?.unfocus();
+  if (email.isEmpty || password.isEmpty) {
+    showSnackbar(context, 'Please Enter both fields');
+  } else {
+    context.read<NotesService>().getNotes(email);
+    Navigator.of(context).popAndPushNamed(RouteManager
+        .noteListPage); //TODO I am not sure if I selected the correct route here, double check for me please.
+  }
+}
 
 void resetPasswordInUI(BuildContext context, {required String email}) async {}
 
